c05acfc680e959f1467b61e04feb64daef72572e
galangriarulhaq <galangrz337@gmail.com>
2024-02-10 00:08:34 +0700

    Testing Project Card

--- src/app/component/TabButton.jsx
+++ src/app/component/TabButton.jsx
@@ -12,7 +12,7 @@ const TabButton = ({ active, selectTab, children }) => {
   return (
     <button onClick={selectTab}>
       <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>{children}</p>
-      <motion.div animate={active ? "active" : "default"} variants={variants} className="h-1 bg-primary-500 mt-2 mr-3"></motion.div>
+      <motion.div animate={active ? "active" : "default"} variants={variants} className="h-1 bg-purple-800 mt-2 mr-3"></motion.div>
     </button>
   );
 };
