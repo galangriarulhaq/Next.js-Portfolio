5eefad10db9bd391af2ec9313e379eda6bfeefa8
galangriarulhaq <galangrz337@gmail.com>
2024-02-14 01:10:10 +0700

    Styling HeroSection

--- src/app/component/HeroSection.jsx
+++ src/app/component/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className="col-span-8 place-self-center text-center sm:text-left justify-self-start md:px-24">
           <h1 className="text-white mb-4 text-2xl sm:text-3xl lg:text-5xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-purple-900">Hello, I&apos;m </span>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-900 to-violet-950">Hello, I&apos;m </span>
             <br></br>
             <TypeAnimation sequence={["Galang Riarulhaq", 2000, "Student", 2000, "Web Developer", 1000, "Mobile Developer", 2000]} wrapper="span" speed={50} repeat={Infinity} />
           </h1>
@@ -19,11 +19,11 @@ const HeroSection = () => {
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-white to-purple-900 hover:bg-slate-200 text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-fuchsia-900 to-violet-950 hover:bg-slate-200 text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
             >
               Hire Me
             </Link>
-            <Link href="/" className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-white to-purple-900 text-white mt-3 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+            <Link href="/" className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-fuchsia-900 to-violet-950 text-white mt-3 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
               <span className="block bg-[#121212] rounded-full px-5 py-2">Download CV</span>
             </Link>
           </div>
