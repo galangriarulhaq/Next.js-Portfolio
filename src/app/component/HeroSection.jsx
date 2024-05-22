7a2a63002933f67b3f9d853780e2e6f71c32f0a9
galangriarulhaq <galangrz337@gmail.com>
2024-05-22 22:16:22 +0700

    Modified HeroSection

--- src/app/component/HeroSection.jsx
+++ src/app/component/HeroSection.jsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
         </motion.div>
         <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className="col-span-4 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image src="" alt="hero image" className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" width={300} height={300} />
+            <Image src="/me_.png" alt="hero image" className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" width={320} height={320} />
           </div>
         </motion.div>
       </div>
